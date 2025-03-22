9c41ac348105a3bb7ea437bbcc8e43ed259b3cb6
Hert00l <fortkebab84@gmail.com>
2025-03-22 17:23:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
 
   return (
     <Router>
-      <div className="min-h-screen bg-black text-white bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-900/40 via-black to-black">
+      <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] text-white bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-900/40 via-[#101010] to-[#101010]
+">
         {loading && <LoadingScreen onComplete={() => setLoading(false)} />}
         <Navbar />
         <Routes>
